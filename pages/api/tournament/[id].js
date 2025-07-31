729ba288bccecc68eb9f494804ffb587839de17e
Xinyu Tu <carson_tu@hotmail.com>
2025-07-31 14:15:09 -0400

    4

--- pages/api/tournament/[id].js
+++ pages/api/tournament/[id].js
@@ -1,6 +1,6 @@
 // pages/api/tournament/[id].js
-import dbConnect from '@/lib/mongodb.js';
-import Tournament from '@/models/Tournament';
+import dbConnect from '../../../lib/mongodb.js';
+import Tournament from '../../../models/Tournament.js';
 
 export default async function handler(req, res) {
   const { method } = req;
